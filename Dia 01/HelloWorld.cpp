fc3d005b05f1577151445dc08820c871b5bd3f75
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-07 16:40:50 -0300

    Hello world em c++, entendendo os primeiros conceitos da linguagem

---
+++ Dia 01/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+
+    // This is a comment
+    /*
+       This
+       is
+       a
+       multi-line
+       comment
+    */
+    std::cout << "Hello World" << '\n';
+    std::cout << "Progaming in c++ it's really good" << '\n';
+    return 0;
+}
