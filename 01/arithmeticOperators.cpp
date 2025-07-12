e4da1f54634697ce8bf8c44f9bae612df974766a
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-12 08:47:57 -0300

    Estudo sobre operadores aritméticos

---
+++ 01/arithmeticOperators.cpp
@@ -0,0 +1,19 @@
+// C++ Arithmetic Operators
+
+#include <iostream>
+
+int main()
+{
+    // Adding two integers using operator '+'
+    int sum = 35 + 72;
+    int diff = 384 - 323;
+
+    std::cout << sum << '\n';
+    std::cout << diff << '\n';
+
+    int students = students + 9;
+
+    std::cout << students;
+
+    return 0;
+}
