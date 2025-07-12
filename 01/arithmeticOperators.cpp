f6c1bb8ffb3ccf000d1935cd58be64feb37bf8d1
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-12 09:55:23 -0300

    Estudo de operadores aritméticos: soma

--- 01/arithmeticOperators.cpp
+++ 01/arithmeticOperators.cpp
@@ -1,6 +1,14 @@
 // C++ Arithmetic Operators
 
 #include <iostream>
+using namespace std;
+
+void addition()
+{
+    int a = 34;
+    int b = 234;
+    cout << "Soma (addition): " << a + b << '\n';
+}
 
 int main()
 {
@@ -8,12 +16,10 @@ int main()
     int sum = 35 + 72;
     int diff = 384 - 323;
 
-    std::cout << sum << '\n';
-    std::cout << diff << '\n';
+    cout << "Soma:    " << sum << '\n';
+    cout << "Diferença: " << diff << '\n';
 
-    int students = students + 9;
-
-    std::cout << students;
+    addition();
 
     return 0;
 }
